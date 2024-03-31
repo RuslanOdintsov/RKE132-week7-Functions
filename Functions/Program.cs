12764fd8eec9c320c07580c18a3a41e03a30fa4e
Ruslan.Odintsov <rodintso@tktk.ee>
2024-03-31 19:30:16 +0300

    week7

---
+++ Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Are you coming or leaving?");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "coming")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+static void PrintHello()
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, KingKong.");
+}
